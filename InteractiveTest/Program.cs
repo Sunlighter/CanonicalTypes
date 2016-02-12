c1da4cd5e763da317c9b3f636b298d053dcb0f71
Sunlighter <edkiser@gmail.com>
2016-02-12 06:16:40 -0800

    Add a simple interactive test

---
+++ InteractiveTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CanonicalTypes;
+
+namespace InteractiveTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.Write("> ");
+                    string str = Console.ReadLine();
+                    Datum d;
+                    if (Datum.TryParse(str, out d))
+                    {
+                        Console.WriteLine("* " + d.ToString());
+                        if (d is SymbolDatum && ((SymbolDatum)d).IsInterned && ((SymbolDatum)d).Name == "exit")
+                            break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parse error");
+                    }
+                }
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc);
+            }
+        }
+    }
+}
