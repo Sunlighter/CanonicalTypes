80f4af9d43b832106d954e3eb836940e9082a4ca
Sunlighter <edkiser@gmail.com>
2024-07-13 23:22:54 -0400

    Add an ObjectIDGenerator class to avoid deprecation issues

---
+++ CanonicalTypes/ObjectIDGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sunlighter.CanonicalTypes
+{
+#if !NETSTANDARD2_0
+    public sealed class ObjectIDGenerator
+    {
+        private readonly object syncRoot;
+        private long nextId;
+        private readonly Dictionary<object, long> dict;
+
+        public ObjectIDGenerator()
+        {
+            syncRoot = new object();
+            nextId = 0L;
+            dict = new Dictionary<object, long>(ReferenceEqualityComparer.Instance);
+        }
+
+        public long GetId(object obj, out bool firstTime)
+        {
+            lock (syncRoot)
+            {
+                if (dict.TryGetValue(obj, out long id))
+                {
+                    firstTime = false;
+                    return id;
+                }
+                else
+                {
+                    long newId = nextId;
+                    ++nextId;
+                    dict.Add(obj, newId);
+                    firstTime = true;
+                    return newId;
+                }
+            }
+        }
+    }
+#endif
+}
